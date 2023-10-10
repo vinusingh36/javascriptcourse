1c50345e8f96547b75e7bab46d41cf2a5365dfb9
vinusingh36 <vineet.vinusingh36@gmail.com>
2023-10-10 23:34:41 +0530

    course template

---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,57 @@
+/*let js = "amazing";
+
+//40 + 8 + 30 - 10;
+console.log(40 + 8 + 30 - 10);
+console.log('jonas');
+console.log(23);
+
+
+let firstName = "bob";
+console.log(firstName);
+
+let name = "vineet";  //not recommended
+console.log(name);
+
+//varaible name is discripted to avoid confusion
+
+let country = "India";
+let continent = "Asia";
+let population = 1.4
+console.log(country, continent, population);
+*/
+//DataTypes
+
+
+//Number-> Floating point number used for decimal and number;
+//String - sequence of character use for text
+//single or double qoute
+//Boolean - true or false
+//Undefined -empty value
+//null -
+//Symbol - unique vlaue that cant be changed.
+//BigInt - large number
+
+
+
+let javascriptIsFun = true;
+// console.log(javascriptIsFun);
+
+// console.log(typeof (true));
+console.log(typeof javascriptIsFun);
+// console.log(typeof 23);
+// console.log(typeof "javascriptIsFun");
+
+javascriptIsFun = "Yes!"
+console.log(javascriptIsFun);
+console.log(typeof javascriptIsFun);
+
+let year;
+console.log(year);
+console.log(typeof year);
+
+year = 1991;
+console.log(year);
+console.log(typeof year);
+
+
+console.log(typeof null);
